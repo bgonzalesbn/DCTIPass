367233b4db458bae7d8e0598563ad3c18e530140
bgonzalezsbn <bgonzalezsbn@gmail.com>
2026-02-12 10:52:48 -0600

    fix: SendGrid import for CommonJS compatibility

--- backend/src/modules/auth/email.service.ts
+++ backend/src/modules/auth/email.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Logger } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import * as sgMail from "@sendgrid/mail";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const sgMail = require("@sendgrid/mail");
 
 @Injectable()
 export class EmailService {
